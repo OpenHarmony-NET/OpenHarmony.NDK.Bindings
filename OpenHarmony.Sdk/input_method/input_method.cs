c4298844ca75dac0750095dd415779c6ff454e18
CeSun <ce.sun@outlook.com>
2024-11-15 12:49:38 +0800

    inputmethod

---
+++ OpenHarmony.Sdk/input_method/input_method.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHarmony.Sdk;
+
+public unsafe static partial class input_method
+{
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_AttachOptions* OH_AttachOptions_Create([MarshalAs(UnmanagedType.Bool)] bool showKeyboard);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial void OH_AttachOptions_Destroy(InputMethod_AttachOptions* options);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_AttachOptions_IsShowKeyboard(InputMethod_AttachOptions* options, bool* showKeyboard);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_ShowKeyboard(InputMethod_InputMethodProxy* inputMethodProxy);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_HideKeyboard(InputMethod_InputMethodProxy* inputMethodProxy);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_NotifySelectionChange(InputMethod_InputMethodProxy* inputMethodProxy, char* text, ulong length, int start, int end);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_NotifyConfigurationChange(InputMethod_InputMethodProxy* inputMethodProxy, InputMethod_EnterKeyType enterKey, InputMethod_TextInputType textType);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_NotifyCursorUpdate(InputMethod_InputMethodProxy* inputMethodProxy, InputMethod_CursorInfo* cursorInfo);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodProxy_SendPrivateCommand(InputMethod_InputMethodProxy* inputMethodProxy, InputMethod_PrivateCommand** privateCommand, ulong size);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodController_Attach(InputMethod_TextEditorProxy* textEditorProxy, InputMethod_AttachOptions* options, InputMethod_InputMethodProxy** inputMethodProxy);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_InputMethodController_Detach(InputMethod_InputMethodProxy* inputMethodProxy);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_CursorInfo* OH_CursorInfo_Create(double left, double top, double width, double height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial void OH_CursorInfo_Destroy(InputMethod_CursorInfo* cursorInfo);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_CursorInfo_SetRect(InputMethod_CursorInfo* cursorInfo, double left, double top, double width, double height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_CursorInfo_GetRect(InputMethod_CursorInfo* cursorInfo, double* left, double* top, double* width, double* height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_PrivateCommand* OH_PrivateCommand_Create(sbyte* key, ulong keyLength);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial void OH_PrivateCommand_Destroy(InputMethod_PrivateCommand* command);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_SetKey(InputMethod_PrivateCommand* command, sbyte* key, ulong keyLength);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_SetBoolValue(InputMethod_PrivateCommand* command, [MarshalAs(UnmanagedType.Bool)] bool value);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_SetIntValue(InputMethod_PrivateCommand* command, int value);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_SetStrValue(InputMethod_PrivateCommand* command, sbyte* value, ulong valueLength);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_GetKey(InputMethod_PrivateCommand* command, sbyte** key, ulong* keyLength);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_GetValueType(InputMethod_PrivateCommand* command, InputMethod_CommandValueType* type);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_GetBoolValue(InputMethod_PrivateCommand* command, bool* value);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_GetIntValue(InputMethod_PrivateCommand* command, int* value);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_PrivateCommand_GetStrValue(InputMethod_PrivateCommand* command, sbyte** value, ulong* valueLength);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_TextAvoidInfo* OH_TextAvoidInfo_Create(double positionY, double height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial void OH_TextAvoidInfo_Destroy(InputMethod_TextAvoidInfo* info);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextAvoidInfo_SetPositionY(InputMethod_TextAvoidInfo* info, double positionY);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextAvoidInfo_SetHeight(InputMethod_TextAvoidInfo* info, double height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextAvoidInfo_GetPositionY(InputMethod_TextAvoidInfo* info, double* positionY);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextAvoidInfo_GetHeight(InputMethod_TextAvoidInfo* info, double* height);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_TextConfig* OH_TextConfig_Create();
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial void OH_TextConfig_Destroy(InputMethod_TextConfig* config);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_SetInputType(InputMethod_TextConfig* config, InputMethod_TextInputType inputType);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_SetEnterKeyType(InputMethod_TextConfig* config, InputMethod_EnterKeyType enterKeyType);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_SetPreviewTextSupport(InputMethod_TextConfig* config, [MarshalAs(UnmanagedType.Bool)] bool supported);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_SetSelection(InputMethod_TextConfig* config, int start, int end);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_SetWindowId(InputMethod_TextConfig* config, int windowId);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetInputType(InputMethod_TextConfig* config, InputMethod_TextInputType* inputType);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetEnterKeyType(InputMethod_TextConfig* config, InputMethod_EnterKeyType* enterKeyType);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_IsPreviewTextSupported(InputMethod_TextConfig* config, bool* supported);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetCursorInfo(InputMethod_TextConfig* config, InputMethod_CursorInfo** cursorInfo);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetTextAvoidInfo(InputMethod_TextConfig* config, InputMethod_TextAvoidInfo** avoidInfo);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetSelection(InputMethod_TextConfig* config, int* start, int* end);
+
+    [LibraryImport("libohinputmethod.so")]
+    public static partial InputMethod_ErrorCode OH_TextConfig_GetWindowId(InputMethod_TextConfig* config, int* windowId);
+
+}
---
+++ OpenHarmony.Sdk/input_method/input_method_type.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHarmony.Sdk;
+
+public struct InputMethod_AttachOptions
+{
+
+}
+
+public struct InputMethod_TextEditorProxy
+{
+
+}
+public struct InputMethod_InputMethodProxy
+{
+
+}
+public struct InputMethod_CursorInfo
+{
+
+}
+
+public struct InputMethod_PrivateCommand
+{
+
+}
+
+public struct InputMethod_TextAvoidInfo
+{
+
+}
+
+public struct InputMethod_TextConfig
+{
+
+}
+public enum InputMethod_CommandValueType
+{
+    /**
+     * Value type is NONE.
+     */
+    IME_COMMAND_VALUE_TYPE_NONE = 0,
+    /**
+     * Value type is STRING.
+     */
+    IME_COMMAND_VALUE_TYPE_STRING = 1,
+    /**
+     * Value type is BOOL.
+     */
+    IME_COMMAND_VALUE_TYPE_BOOL = 2,
+    /**
+     * Value type is INT32.
+     */
+    IME_COMMAND_VALUE_TYPE_INT32 = 3,
+}
+
+public enum InputMethod_EnterKeyType
+{
+    /**
+     * The enter key type is UNSPECIFIED.
+     */
+    IME_ENTER_KEY_UNSPECIFIED = 0,
+    /**
+     * The enter key type is NONE.
+     */
+    IME_ENTER_KEY_NONE = 1,
+    /**
+     * The enter key type is GO.
+     */
+    IME_ENTER_KEY_GO = 2,
+    /**
+     * The enter key type is SEARCH.
+     */
+    IME_ENTER_KEY_SEARCH = 3,
+    /**
+     * The enter key type is SEND.
+     */
+    IME_ENTER_KEY_SEND = 4,
+    /**
+     * The enter key type is NEXT.
+     */
+    IME_ENTER_KEY_NEXT = 5,
+    /**
+     * The enter key type is DONE.
+     */
+    IME_ENTER_KEY_DONE = 6,
+    /**
+     * The enter key type is PREVIOUS.
+     */
+    IME_ENTER_KEY_PREVIOUS = 7,
+    /**
+     * The enter key type is NEWLINE.
+     */
+    IME_ENTER_KEY_NEWLINE = 8,
+}
+
+public enum InputMethod_TextInputType
+{
+    /**
+     * The text input type is NONE.
+     */
+    IME_TEXT_INPUT_TYPE_NONE = -1,
+    /**
+     * The text input type is TEXT.
+     */
+    IME_TEXT_INPUT_TYPE_TEXT = 0,
+    /**
+     * The text input type is MULTILINE.
+     */
+    IME_TEXT_INPUT_TYPE_MULTILINE = 1,
+    /**
+     * The text input type is NUMBER.
+     */
+    IME_TEXT_INPUT_TYPE_NUMBER = 2,
+    /**
+     * The text input type is PHONE.
+     */
+    IME_TEXT_INPUT_TYPE_PHONE = 3,
+    /**
+     * The text input type is DATETIME.
+     */
+    IME_TEXT_INPUT_TYPE_DATETIME = 4,
+    /**
+     * The text input type is EMAIL ADDRESS.
+     */
+    IME_TEXT_INPUT_TYPE_EMAIL_ADDRESS = 5,
+    /**
+     * The text input type is URL.
+     */
+    IME_TEXT_INPUT_TYPE_URL = 6,
+    /**
+     * The text input type is VISIBLE PASSWORD.
+     */
+    IME_TEXT_INPUT_TYPE_VISIBLE_PASSWORD = 7,
+    /**
+     * The text input type is NUMBER PASSWORD.
+     */
+    IME_TEXT_INPUT_TYPE_NUMBER_PASSWORD = 8,
+    /**
+     * The text input type is SCREEN LOCK PASSWORD.
+     */
+    IME_TEXT_INPUT_TYPE_SCREEN_LOCK_PASSWORD = 9,
+    /**
+     * The text input type is USER NAME.
+     */
+    IME_TEXT_INPUT_TYPE_USER_NAME = 10,
+    /**
+     * The text input type is NEW PASSWORD.
+     */
+    IME_TEXT_INPUT_TYPE_NEW_PASSWORD = 11,
+    /**
+     * The text input type is NUMBER DECIMAL.
+     */
+    IME_TEXT_INPUT_TYPE_NUMBER_DECIMAL = 12,
+}
+public enum InputMethod_ErrorCode
+{
+    /**
+     * @error The error code in the correct case.
+     */
+    IME_ERR_OK = 0,
+
+    /**
+     * @error The error code when error is undefined.
+     */
+    IME_ERR_UNDEFINED = 1,
+    /**
+     * @error The error code when parameter check failed.
+     */
+    IME_ERR_PARAMCHECK = 401,
+    /**
+     * @error The error code when the package manager error.
+     */
+    IME_ERR_PACKAGEMANAGER = 12800001,
+    /**
+     * @error The error code when input method engine error.
+     */
+    IME_ERR_IMENGINE = 12800002,
+    /**
+     * @error The error code when input method client error.
+     */
+    IME_ERR_IMCLIENT = 12800003,
+    /**
+     * @error The error code when configuration persisting error.
+     */
+    IME_ERR_CONFIG_PERSIST = 12800005,
+    /**
+     * @error The error code when input method controller error.
+     */
+    IME_ERR_CONTROLLER = 12800006,
+    /**
+     * @error The error code when input method setting error.
+     */
+    IME_ERR_SETTINGS = 12800007,
+    /**
+     * @error The error code when input method manager service error.
+     */
+    IME_ERR_IMMS = 12800008,
+    /**
+     * @error The error code when input method client is detached.
+     */
+    IME_ERR_DETACHED = 12800009,
+    /**
+     * @error The error code when unexpected null pointer.
+     */
+    IME_ERR_NULL_POINTER = 12802000,
+    /**
+     * @error The error code when query failed.
+     */
+    IME_ERR_QUERY_FAILED = 12802001,
+}
