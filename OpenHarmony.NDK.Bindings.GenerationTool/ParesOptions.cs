64636006a29c636042e415c64ba4ba52f7e7c79c
望尘空忧 <wckfxxjstdwckyxphdhqxg@outlook.com>
2025-05-17 00:11:56 +0800

    · 使用CppSharp生成HarmonyOS JSVM模块的绑定。

---
+++ OpenHarmony.NDK.Bindings.GenerationTool/ParesOptions.cs
@@ -0,0 +1,44 @@
+﻿using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+using CppSharp.Passes;
+
+namespace OpenHarmony.NDK.Bindings.GenerationTool;
+
+public class ParesOptions : ILibrary
+{
+    public ParesOptions(string headerFileDirPath)
+    {
+        HeaderFileDirPath = headerFileDirPath;
+    }
+
+    public string HeaderFileDirPath { get; }
+
+    public void Preprocess(Driver driver, ASTContext ctx)
+    {
+    }
+
+    public void Postprocess(Driver driver, ASTContext ctx)
+    {
+    }
+
+    public void Setup(Driver driver)
+    {
+        // driver.ParserOptions.TargetTriple = "x86_64-linux-gnu";
+        var options = driver.Options;
+        options.OutputDir = "../../../../OpenHarmony.NDK.Bindings";
+        options.GeneratorKind = GeneratorKind.CSharp;
+        var module = options.AddModule("libjsvm");
+        var pathBuf = Path.Combine(HeaderFileDirPath, @"ark_runtime\jsvm");
+        module.IncludeDirs.Add(pathBuf);
+        module.Headers.Add(Path.Combine(pathBuf, "jsvm.h"));
+        module.Headers.Add(Path.Combine(pathBuf, "jsvm_types.h"));
+        module.OutputNamespace = "OpenHarmony.NDK.Bindings";
+    }
+
+    public void SetupPasses(Driver driver)
+    {
+        driver.Context.TranslationUnitPasses.RenameDeclsUpperCase(RenameTargets.Any);
+        driver.Context.TranslationUnitPasses.AddPass(new FunctionToInstanceMethodPass());
+    }
+}
